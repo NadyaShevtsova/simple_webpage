e4082de4cb10a9d87925a3d9a9e2b8bf4d55f31c
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2015-10-12 22:07:33 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Organization.create ([
+  { title: "ABC Company", location: "Sydney" },
+  { title: "XYZ Company", location: "Brisbane" },
+  { title: "Company ABC XYZ", location: "Melbourne"}
+])
+
+Person.create([
+  { first_name: "Paul", last_name: "Smith", organization: Organization.first, assessement_count: 1},
+  { first_name: "Andrew", last_name: "Baker", organization: Organization.find(2), assessement_count: 0},
+  { first_name: "Michael", last_name: "Clarke", organization: Organization.last, assessement_count: 14},
+  { first_name: "David", last_name: "Cline", organization: Organization.first, assessement_count: 3},
+  { first_name: "Adam", last_name: "Broadbent", organization: Organization.find(2), assessement_count: 1}
+])
