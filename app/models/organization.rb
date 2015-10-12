316d9d3b59c15261f326201e31e430972302462f
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2015-10-12 11:19:24 +0300

    modified models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ActiveRecord::Base
+  has_many :persons, dependent: :destroy
+
   with_options presence: true do |v|
     v.validates :location
     v.validates :title, length: { maximum: 255 }
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,10 @@
 class Person < ActiveRecord::Base
+  belongs_to :organization
+  validates :organization, presence: true
+
+  with_options presence: true do |p|
+    p.validates :first_name, length: { maximum: 255 }
+    p.validates :last_name, length: { maximum: 255 }
+  end
+  validates :assessement_count, numericality: { only_integer: true, greater_then_or_equal_to: 0 }
 end
