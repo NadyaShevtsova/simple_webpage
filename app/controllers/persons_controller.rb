01f2036205d51f43be0b38ae41054eba60a60af6
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2015-10-16 11:17:15 +0300

    added show page to person

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -4,6 +4,10 @@ class PersonsController < ApplicationController
     @persons = @q.result(distinct: true)
   end
 
+  def show
+    @person = Person.find(params[:id])
+  end
+
 private
   def person_params
     params.require(:person).permit(:first_name, :last_name)
